98743564eed84117f08e1963b984d908da27e86f
Rohan Bavishi <rbavishi@berkeley.edu>
2022-01-26 10:17:54 -0800

    Add test for bullet-point NL to code

--- tests/datana/test_nl2code.py
+++ tests/datana/test_nl2code.py
@@ -41,3 +41,50 @@ class NL2CodeTests(unittest.TestCase):
         exec(generated_code, ctx)
         self.assertEqual(5, ctx['func'](10, 5))  # 10 - 5 = 5
         self.assertEqual(-15, ctx['func'](5, 20))  # 5 - 20 = -15
+
+    def test_2(self):
+        few_shot_examples = [
+            nl2code.FewShotExample(
+                nl=[
+                    "A function that takes two integers as input",
+                    "Returns their xor"
+                ],
+                code=(
+                    "def f(a, b):\n"
+                    "    return a ^ b"
+                )
+            ),
+            nl2code.FewShotExample(
+                nl=[
+                    "A function that takes two integers as input",
+                    "Returns their product"
+                ],
+                code=(
+                    "def f(a, b):\n"
+                    "    return a * b"
+                )
+            ),
+        ]
+
+        target_nl = [
+            "A function that takes two strings as input",
+            "Returns their concatenation"
+        ]
+        generator = nl2code.SimpleNatLangToCode()
+        generated_code = generator.get_code(few_shot_examples, target_nl)
+
+        #  Run the generated code to see if it does the right thing
+        ctx = {}
+        exec(generated_code, ctx)
+        self.assertEqual("ab", ctx['f']("a", "b"))
+        self.assertEqual("10", ctx['f']("1", "0"))
+
+        #  Try with output prefixes. Use specific variable names.
+        output_prefix = "def func(x, y):\n"
+        generated_code = generator.get_code(few_shot_examples, target_nl, output_prefix)
+
+        #  Run the generated code to see if it does the right thing
+        ctx = {}
+        exec(generated_code, ctx)
+        self.assertEqual("ab", ctx['func']("a", "b"))
+        self.assertEqual("10", ctx['func']("1", "0"))
