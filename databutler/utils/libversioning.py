09904b05854302f92e812abcd7ec3620cf22ebcd
Rohan Bavishi <rbavishi@berkeley.edu>
2022-01-31 09:09:43 -0800

    Add documentation for lib-versioning utils

--- databutler/utils/libversioning.py
+++ databutler/utils/libversioning.py
@@ -7,16 +7,37 @@ from databutler.utils import paths as path_utils
 
 
 def _get_lib_version_path(lib_name: str, version: str) -> str:
+    """
+    Get the pre-defined path for the library version.
+
+    Args:
+        lib_name (str): Name of the library.
+        version (str): The version to use. This must be an exact version - wildcards are not supported as of now.
+    """
     path = os.path.join(path_utils.get_user_home_dir_path(), ".databutler", f"{lib_name}-{version}")
     return path
 
 
 def _check_if_lib_version_exists(lib_name: str, version: str) -> bool:
+    """
+    Check if the specified library version is already installed at the pre-defined path.
+
+    Args:
+        lib_name (str): Name of the library.
+        version (str): The version to use. This must be an exact version - wildcards are not supported as of now.
+    """
     path = _get_lib_version_path(lib_name, version)
     return os.path.exists(path)
 
 
 def _download_lib_version(lib_name: str, version: str) -> None:
+    """
+    Installs the specified version at the pre-defined path for library versions.
+
+    Args:
+        lib_name (str): Name of the library.
+        version (str): The version to use. This must be an exact version - wildcards are not supported as of now.
+    """
     path = _get_lib_version_path(lib_name, version)
 
     #  As per guidelines at https://pip.pypa.io/en/latest/user_guide/#using-pip-from-your-program
@@ -27,6 +48,29 @@ def _download_lib_version(lib_name: str, version: str) -> None:
 
 @contextmanager
 def modified_lib_env(lib_name: str, version: str):
+    """
+    Temporarily modifies sys.path to allow a different library version to be picked up.
+
+    For best results, this should be run in a different process. Note that the process must be spawned, not forked.
+    Since fork is the default on Unix, it might fail on Unix systems if the spawn multiprocess context is not specified.
+
+    Mote that tests/runs etc. will probably fail with PyCharm. It seems to be modifying sys.path by itself or
+    influencing the import process somehow.
+
+    For multiple versions, chain the contexts together, as shown below:
+
+    ```
+    with modified_lib_env("seaborn", "0.11"), modified_lib_env("pandas", "0.25.0"):
+        [your code here]
+    ```
+
+    This further means that the order must be carefully chosen by the user. The outer context call libraries will be
+    added first to sys.path, and hence the libraries added later will be picked up first (stack model).
+
+    Args:
+        lib_name (str): Name of the library.
+        version (str): The version to use. This must be an exact version - wildcards are not supported as of now.
+    """
     #  First ensure the specific version is downloaded.
     if not _check_if_lib_version_exists(lib_name, version):
         _download_lib_version(lib_name, version)
--- databutler/utils/multiprocess.py
+++ databutler/utils/multiprocess.py
@@ -8,8 +8,23 @@ class FuncTimeoutError(TimeoutError):
     pass
 
 
-def run_func_in_process(func: Callable, *args, _timeout: Optional[int] = None, **kwargs):
-    c_func = concurrent.process(timeout=_timeout, context=mp.get_context('spawn'))(func)
+def run_func_in_process(func: Callable, *args, _timeout: Optional[int] = None, _use_spawn: bool = True, **kwargs):
+    """
+    Runs the provided function in a separate process with the supplied args and kwargs. The args, kwargs, and
+    return values must all be pickle-able.
+
+    Args:
+        func: The function to run.
+        *args: Positional args, if any.
+        _timeout: A timeout to use for the function.
+        _use_spawn: The 'spawn' multiprocess context is used if True. 'fork' is used otherwise.
+        **kwargs: Keyword args, if any.
+
+    Returns:
+        The result of executing the function.
+    """
+    mode = 'spawn' if _use_spawn else 'fork'
+    c_func = concurrent.process(timeout=_timeout, context=mp.get_context(mode))(func)
     future = c_func(*args, **kwargs)
 
     try:
