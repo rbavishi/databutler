b47cd01111ec48650169434f74f06026ebbfdb1d
Rohan Bavishi <rbavishi@berkeley.edu>
2022-02-01 11:59:51 -0800

    Add simple loguru based stderr and file logger

--- databutler/utils/paths.py
+++ databutler/utils/paths.py
@@ -4,3 +4,7 @@ import pathlib
 
 def get_user_home_dir_path() -> str:
     return str(pathlib.Path.home().absolute())
+
+
+def get_logging_dir_path() -> str:
+    return os.path.join(get_user_home_dir_path(), ".databutler", "logs")
