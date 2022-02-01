57fa0afa4fdbe3dba07b4ff92b149a6966c6669d
Arushi Somani <arushisomani@berkeley.edu>
2022-02-01 08:10:25 -0800

    removing amks/test files

--- tests/datana/test_code2nl.py
+++ tests/datana/test_code2nl.py
@@ -1,12 +1,11 @@
 import itertools
 import unittest
 
-from databutler.datana.training import code2nl, few_shot, nl2code
-from databutler.datana.utils import vizutils
+from databutler.datana.generic.training import code2nl, few_shot, nl2code
 
 
 class Code2NLTests(unittest.TestCase):
-    def test_simple_1(self):
+    def test_1(self):
         few_shot_examples = [
             few_shot.FewShotExampleCodeAndNL(
                 nl="A function to add two numbers",
@@ -45,7 +44,7 @@ class Code2NLTests(unittest.TestCase):
         self.assertEqual(5 & 10, ctx['f'](10, 5))
         self.assertEqual(5 & 20, ctx['f'](5, 20))
 
-    def test_simple_2(self):
+    def test_2(self):
         #  Same as test_1, but we use 2 bullet points.
         few_shot_examples = [
             few_shot.FewShotExampleCodeAndNL(
@@ -93,133 +92,3 @@ class Code2NLTests(unittest.TestCase):
         self.assertIn('f', ctx.keys())
         self.assertEqual(5 & 10, ctx['f'](10, 5))
         self.assertEqual(5 & 20, ctx['f'](5, 20))
-
-    def test_vizfunc_1(self):
-        """
-        Tests to generate basic visualization functions.
-        """
-        import numpy as np
-        few_shot_examples = [
-            few_shot.FewShotExampleCodeAndNL(
-                nl = [
-                    "Defines a function f that accepts a dataframe df, the column name x, and the column name y",
-                    "Plot the y column of the dataframe against the x column"
-                ],
-                code = (
-                    "import matplotlib.pyplot as plt\n"
-                    "def f(df, x, y):\n"
-                    "   return plt.plot(df[x], df[y])"
-                )
-            ),
-            few_shot.FewShotExampleCodeAndNL(
-                nl = [
-                    "Defines a function f that accepts a dataframe df, the column name x and the column name y, and a list of labels",
-                    "Create a stacked plot the y column against the x column, with the labels as the stacks",
-                ],
-                code = (
-                    "import matplotlib.pyplot as plt\n"
-                    "def f(df, x, y, labels):\n"
-                    "   plt.legend(loc='upper left')\n"
-                    "   return plt.stackplot(df[x], df[y], labels=labels)"
-                )
-            )
-        ]
-
-        target_code = (
-            "import matplotlib.pyplot as plt\n"
-            "def f(df, col):\n"
-            "    return plt.hist(df[col])\n"
-        )
-
-        nl_generator = code2nl.SimpleCodeToNatLang()
-        code_generator = nl2code.SimpleNatLangToCode()
-
-        #  The generated NL should be something like "A function to create a histogram..."
-        generated_nl = list(itertools.islice(nl_generator.get_nl_bullets(few_shot_examples, target_code), 2))
-        self.assertIsInstance(generated_nl, list)
-        self.assertEqual(2, len(generated_nl))
-
-        regenerated_code = code_generator.get_code(few_shot_examples, generated_nl)
-
-        # Setup for inputs to the code
-        df = {'input': [1, 2, 3, 4, 4, 5, 5, 5]}
-
-        # Run the original code and save the plot
-        ctx = {}
-        exec(target_code, ctx)
-        self.assertIn('f', ctx.keys())
-        target_value, target_bins, _ = ctx['f'](df, 'input')
-
-        # Run the regenerated code and save the plot
-        ctx = {}
-        exec(regenerated_code, ctx)
-        self.assertIn('f', ctx.keys())
-        generated_value, generated_bins, _ = ctx['f'](df, 'input')
-
-        # Compare the two
-        # Checks if the values of the histogram bins are equal
-        self.assertTrue(np.array_equal(target_value, generated_value))
-        # Checks if the edges of the bins are equal
-        self.assertTrue(np.array_equal(target_bins, generated_bins))
-
-    # Same as test_vizfunc_1 but instead uses image bytestream comparison to check equality.
-    def test_vizfunc_2(self):
-        """
-        Tests to generate basic visualization functions.
-        """
-        import numpy as np
-        few_shot_examples = [
-            few_shot.FewShotExampleCodeAndNL(
-                nl = [
-                    "Defines a function f that accepts a dataframe df, the column name x, and the column name y",
-                    "Plot the y column of the dataframe against the x column"
-                ],
-                code = (
-                    "import matplotlib.pyplot as plt\n"
-                    "def f(df, x, y):\n"
-                    "   plt.plot(df[x], df[y])"
-                )
-            ),
-            few_shot.FewShotExampleCodeAndNL(
-                nl = [
-                    "Defines a function f that accepts a dataframe df, the column name x and the column name y, and a list of labels",
-                    "Create a stacked plot the y column against the x column, with the labels as the stacks",
-                ],
-                code = (
-                    "import matplotlib.pyplot as plt\n"
-                    "def f(df, x, y, labels):\n"
-                    "   plt.legend(loc='upper left')\n"
-                    "   plt.stackplot(df[x], df[y], labels=labels)"
-                )
-            )
-        ]
-
-        target_code = (
-            "import matplotlib.pyplot as plt\n"
-            "def f(df, col):\n"
-            "   plt.hist(df[col])"
-        )
-
-        nl_generator = code2nl.SimpleCodeToNatLang()
-        code_generator = nl2code.SimpleNatLangToCode()
-
-        #  The generated NL should be something like "A function to create a histogram..."
-        generated_nl = list(itertools.islice(nl_generator.get_nl_bullets(few_shot_examples, target_code), 2))
-        self.assertIsInstance(generated_nl, list)
-        self.assertEqual(2, len(generated_nl))
-
-        regenerated_code = code_generator.get_code(few_shot_examples, generated_nl)
-
-        # Setup for inputs to the code
-        df = {'input': [1, 2, 3, 4, 4, 5, 5, 5]}
-
-        target_fig = vizutils.run_viz_code_matplotlib_mp(target_code, {'df': df, 'col': 'input'}, 'f')
-        generated_fig = vizutils.run_viz_code_matplotlib_mp(regenerated_code, {'df': df, 'col': 'input'}, 'f')
-
-        # Compare the two
-        self.assertTrue(vizutils.serialize_fig(target_fig) == vizutils.serialize_fig(generated_fig))
-
-
-
-
-
--- tests/datana/test_nl2code.py
+++ tests/datana/test_nl2code.py
@@ -1,11 +1,10 @@
 import unittest
 
-from databutler.datana.training import nl2code, few_shot
-from tests.datana.utils.utils import read_file
+from databutler.datana.generic.training import few_shot, nl2code
 
 
 class NL2CodeTests(unittest.TestCase):
-    def test_simple_1(self):
+    def test_1(self):
         few_shot_examples = [
             few_shot.FewShotExampleCodeAndNL(
                 nl="A function to add two numbers",
@@ -45,7 +44,7 @@ class NL2CodeTests(unittest.TestCase):
         self.assertEqual(5, ctx['func'](10, 5))  # 10 - 5 = 5
         self.assertEqual(-15, ctx['func'](5, 20))  # 5 - 20 = -15
 
-    def test_simple_2(self):
+    def test_2(self):
         few_shot_examples = [
             few_shot.FewShotExampleCodeAndNL(
                 nl=[
@@ -91,187 +90,3 @@ class NL2CodeTests(unittest.TestCase):
         exec(generated_code, ctx)
         self.assertEqual("ab", ctx['func']("a", "b"))
         self.assertEqual("10", ctx['func']("1", "0"))
-
-    def test_recursive_1(self):
-        few_shot_examples = [
-            few_shot.FewShotExampleCodeAndNL(
-                nl=[
-                    "A function that takes a number as input",
-                    "Returns that factorial of that number"
-                ],
-                code=(
-                    "def f(n):\n"
-                    "   if n == 1:\n"
-                    "       return 1\n"
-                    "   else:\n"
-                    "       return n * f(n-1)"
-                )
-            ),
-            few_shot.FewShotExampleCodeAndNL(
-                nl=[
-                    "A function that takes a number as input",
-                    "Returns the sum of the digits of the number"
-                ],
-                code=(
-                    "def f(n):\n"
-                    "   if n < 10:\n"
-                    "       return n\n"
-                    "   else:\n"
-                    "       return f(n // 10) + n % 10"
-                )
-            ),
-        ]
-
-        target_nl = [
-            "A function that takes in a number as input",
-            "Returns that term of the fibonacci sequence"
-        ]
-        generator = nl2code.SimpleNatLangToCode()
-        generated_code = generator.get_code(few_shot_examples, target_nl)
-
-        # Run the generated code to see if it does the right thing
-        ctx = {}
-        exec(generated_code, ctx)
-
-        self.assertIn('f', ctx)
-
-        # Edge Cases
-        self.assertEqual(0, ctx['f'](0))
-        self.assertEqual(1, ctx['f'](1))
-
-        # General Cases
-        self.assertEqual(5, ctx['f'](5))
-        self.assertEqual(34, ctx['f'](9))
-        self.assertEqual(4181, ctx['f'](19))
-
-    def test_recursive_2(self):
-        few_shot_examples = [
-            few_shot.FewShotExampleCodeAndNL(
-                nl=[
-                    "A function that takes a number as input",
-                    "Returns that factorial of that number"
-                ],
-                code=(
-                    "def f(n):\n"
-                    "   if n == 1:\n"
-                    "       return 1\n"
-                    "   else:\n"
-                    "       return n * f(n-1)"
-                )
-            ),
-            few_shot.FewShotExampleCodeAndNL(
-                nl=[
-                    "A function that takes a number as input",
-                    "Returns the sum of the digits of the number"
-                ],
-                code=(
-                    "def f(n):\n"
-                    "   if n < 10:\n"
-                    "       return n\n"
-                    "   else:\n"
-                    "       return f(n // 10) + n % 10"
-                )
-            ),
-        ]
-
-        target_nl = [
-            "A function that takes in two non-negative numbers, n and m, as input",
-            "Returns the number of different partitions of n using parts up to m"
-        ]
-        generator = nl2code.SimpleNatLangToCode()
-        generated_code = generator.get_code(few_shot_examples, target_nl)
-
-        # Run the generated code to see if it does the right thing
-        ctx = {}
-        exec(generated_code, ctx)
-
-        self.assertIn('f', ctx)
-
-        # Edge Cases
-        self.assertEqual(1, ctx['f'](0, 5))
-        self.assertEqual(0, ctx['f'](5, 0))
-
-        # General Cases
-        self.assertEqual(9, ctx['f'](6, 4))
-        self.assertEqual(7, ctx['f'](5, 5))
-        self.assertEqual(627, ctx['f'](20, 20))
-
-    def test_datastructs_1(self):
-        """
-        Tests creation of stack data structure with push and pop features.
-        """
-        test_files_loc = 'tests/datana/test_files/datastructs_'
-        few_shot_examples = [
-            few_shot.FewShotExampleCodeAndNL(
-                nl=[
-                    "Defines a class that implements the queue data structure"
-                ],
-                code= read_file(f'{test_files_loc}queue')
-            ),
-            few_shot.FewShotExampleCodeAndNL(
-                nl=[
-                    "Defines a class that implements the priority queue data structure"
-                ],
-                code= read_file(f'{test_files_loc}prioqueue')
-            ),
-        ]
-
-        target_nl = [
-            "Defines a class that implements the stack data structure"
-        ]
-        generator = nl2code.SimpleNatLangToCode()
-        generated_code = generator.get_code(few_shot_examples, target_nl)
-
-        # Run the generated code to see if it does the right thing
-        ctx = {}
-        exec(generated_code, ctx)
-
-        self.assertIn('Stack', ctx)
-        stack = ctx['Stack']()
-
-        # Data structure tests
-        self.assertEqual(True, stack.isEmpty())
-        stack.push(1)
-        self.assertEqual(False, stack.isEmpty())
-        self.assertEqual(1, stack.pop())
-        self.assertEqual(True, stack.isEmpty())
-
-    def test_datastructs_2(self):
-        """
-        Tests creation of priority queue data structure that accepts a priority function with push and pop features.
-        """
-        test_files_loc = 'tests/datana/test_files/datastructs_'
-        few_shot_examples = [
-            few_shot.FewShotExampleCodeAndNL(
-                nl = ["Defines a class that implements the queue data structure"],
-                code = read_file(f'{test_files_loc}queue')
-            ),
-            few_shot.FewShotExampleCodeAndNL(
-                nl = ["Defines a class that implements the stack data structure"],
-                code = read_file(f'{test_files_loc}stack')
-            ),
-        ]
-
-        target_nl = [
-            "Defines a class that implements a priority queue data structure that accepts a priority function",
-            "making all the necessary imports"
-        ]
-        generator = nl2code.SimpleNatLangToCode()
-
-        generated_code = generator.get_code(few_shot_examples, target_nl)
-
-        # Run the generated code to see if it does the right thing
-        ctx = {}
-        exec(generated_code, ctx)
-
-        self.assertIn('PriorityQueue', ctx)
-        queue = ctx['PriorityQueue'](lambda x: -x)
-
-        # Data structure tests
-        self.assertEqual(True, queue.isEmpty())
-        queue.push(1)
-        self.assertEqual(False, queue.isEmpty())
-        queue.push(2)
-        self.assertEqual(2, queue.pop())
-        self.assertEqual(1, queue.pop())
-        self.assertEqual(True, queue.isEmpty())
