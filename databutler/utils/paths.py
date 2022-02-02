f397f2db0a62fea21b5fc334f83f38941eecf80a
Rohan Bavishi <rbavishi@berkeley.edu>
2022-02-02 12:40:26 -0800

    Add tests for pickleutils

--- databutler/utils/paths.py
+++ databutler/utils/paths.py
@@ -1,5 +1,7 @@
 import os
 import pathlib
+import platform
+import tempfile
 
 
 def get_user_home_dir_path() -> str:
@@ -8,3 +10,8 @@ def get_user_home_dir_path() -> str:
 
 def get_logging_dir_path() -> str:
     return os.path.join(get_user_home_dir_path(), ".databutler", "logs")
+
+
+def get_tmpdir_path() -> str:
+    #  tempfile.gettempdir() returns user-isolated paths on MacOS, which we do not want.
+    return "/tmp" if platform.system().lower() == "darwin" else tempfile.gettempdir()
