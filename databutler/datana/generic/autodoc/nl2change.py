350ace6ca03871589479a08878af28b88fa1e9d8
Rohan Bavishi <rbavishi@berkeley.edu>
2022-02-09 22:28:04 -0800

    Add skeletons for three nl-to-change strategies

--- databutler/datana/generic/autodoc/nl2change.py
+++ databutler/datana/generic/autodoc/nl2change.py
@@ -26,3 +26,46 @@ class BaseNatLangToCodeChange(ABC):
         :param task: A nl-to-code-change task instance.
         :return: A string corresponding to the generated code.
         """
+
+
+@attrs.define(eq=False, repr=False)
+class NatLangToNewCode(BaseNatLangToCodeChange):
+    """
+    A change generation strategy where the model is instructed to generated the new code in its entirety.
+    This is the most demanding strategy of the model as it needs to get all the aspects right.
+    """
+
+    def _create_completion_prompt(self, task: NatLangToCodeChangeTask) -> str:
+        pass
+
+    def get_changed_code(self, task: NatLangToCodeChangeTask) -> str:
+        pass
+
+
+@attrs.define(eq=False, repr=False)
+class NatLangToDiff(BaseNatLangToCodeChange):
+    """
+    A change generation strategy where the model is instructed to simply generate the statement-level diff from the
+    original code. This spares the model from having to recreate or repeat the rest of the code verbatim.
+    """
+
+    def _create_completion_prompt(self, task: NatLangToCodeChangeTask) -> str:
+        pass
+
+    def get_changed_code(self, task: NatLangToCodeChangeTask) -> str:
+        pass
+
+
+@attrs.define(eq=False, repr=False)
+class NatLangToFITB(BaseNatLangToCodeChange):
+    """
+    A change generation strategy where the model is instructed to fill in the blank at the specified position.
+    This can be easier for the model than both NatLangToNewCode and NatLangToDiff as the localization problem is solved
+    at a very fine-grained level for the model.
+    """
+
+    def _create_completion_prompt(self, task: NatLangToCodeChangeTask) -> str:
+        pass
+
+    def get_changed_code(self, task: NatLangToCodeChangeTask) -> str:
+        pass
