8d3d56dabd14c5d139bf8e6f3bf530b076119f8f
Rohan Bavishi <rbavishi@berkeley.edu>
2022-02-27 19:04:35 -0800

    Add PickledMap writers and readers

--- databutler/utils/pickleutils.py
+++ databutler/utils/pickleutils.py
@@ -1,10 +1,13 @@
 import os
 import pickle
-from typing import Any, Sequence, List, Union, Optional, BinaryIO
+from typing import Any, Sequence, List, Union, Optional, BinaryIO, Dict, Mapping, Iterator, Hashable
 
 import attrs
 
 from databutler.utils import lazyobjs
+
+
+_FilePos = int
 
 
 def smart_dump(obj: object, pickle_path: str) -> None:
@@ -61,7 +64,7 @@ class PickledCollectionWriter:
     overwrite_existing: bool = True
 
     _file_obj: BinaryIO = attrs.field(init=False)
-    _offset_map: List[int] = attrs.field(init=False)
+    _offset_map: List[_FilePos] = attrs.field(init=False)
 
     def __attrs_post_init__(self):
         if os.path.exists(self.path):
@@ -150,7 +153,7 @@ class PickledCollectionReader(Sequence):
     use_cache: bool = False
 
     _file_obj: BinaryIO = attrs.field(init=False)
-    _offset_map: List[int] = attrs.field(init=False)
+    _offset_map: List[_FilePos] = attrs.field(init=False)
     _cache: List[Union[Any, _Unloaded]] = attrs.field(init=False)
 
     def __attrs_post_init__(self):
@@ -207,6 +210,176 @@ def delete_pickled_collection(path: str):
     offset_map_path = PickledCollectionWriter.get_offset_map_path(path)
     if os.path.exists(offset_map_path):
         os.unlink(offset_map_path)
+
+
+@attrs.define(eq=False, repr=False)
+class PickledMapWriter:
+    """
+    An update-only pickle based obj writer. Keys cannot be deleted or re-assigned. Note that you cannot read values
+    using the writer. To read values, use the PickledMapReader.
+
+    This is useful to store multiple objects in a single picked file in a way
+    that accessing them does not require loading all the preceding objects.
+
+    Use standard dictionary/map syntax to add entries to the pickled map.
+    It is recommended to use this with a context manager to manage opening and closing as follows:
+
+    ```
+    with pickledutils.PickledMapWriter(path) as writer:
+        writer["key"] = "value"
+    ```
+    """
+    path: str
+    overwrite_existing: bool = True
+
+    _file_obj: BinaryIO = attrs.field(init=False)
+    _keys_offset_map: Dict[Hashable, _FilePos] = attrs.field(init=False)
+
+    def __attrs_post_init__(self):
+        if os.path.exists(self.path):
+            if self.overwrite_existing:
+                mode = "w"
+            else:
+                mode = "a"
+        else:
+            mode = "w"
+
+        self._open(mode)
+
+    def _open(self, mode: str):
+        """
+
+        """
+        keys_offset_map_path = self.get_keys_offset_map_path(self.path)
+        if mode == "a":
+            self._file_obj = open(self.path, "ab")
+            try:
+                with open(keys_offset_map_path, "rb") as f:
+                    self._keys_offset_map = pickle.load(f)
+            except FileNotFoundError:
+                if self._file_obj.tell() != 0:
+                    raise FileNotFoundError("Keys to offset map not found for supplied pickle file. "
+                                            "Did you write it using PickledMapReader?")
+                else:
+                    self._keys_offset_map = {}
+
+        else:
+            self._file_obj = open(self.path, "wb")
+            if os.path.exists(keys_offset_map_path):
+                os.unlink(keys_offset_map_path)
+
+            self._keys_offset_map = {}
+
+    def close(self):
+        """
+
+        """
+        with open(self.get_keys_offset_map_path(self.path), "wb") as f:
+            pickle.dump(self._keys_offset_map, file=f)
+
+        self._file_obj.close()
+
+    def __setitem__(self, key, value):
+        if key in self._keys_offset_map:
+            raise ValueError(f"Key {key} already exists")
+
+        offset = self._file_obj.tell()
+        pickle.dump(value, self._file_obj)
+        self._keys_offset_map[key] = offset
+
+    def __len__(self) -> int:
+        return len(self._keys_offset_map)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    @classmethod
+    def get_keys_offset_map_path(cls, path: str) -> str:
+        return path + ".map"
+
+
+@attrs.define(eq=False, repr=False)
+class PickledMapReader(Mapping):
+    """
+    An update-only pickle based obj reader. Note that you cannot read values
+    using the writer. To read values, use the PickledMapReader.
+
+    Use standard dictionary/map syntax to read entries to the pickled map.
+    It is recommended to use this with a context manager to manage opening and closing as follows:
+
+    ```
+    with pickledutils.PickledMapReader(path) as reader:
+        print(reader["key"])
+    ```
+    """
+
+    path: str
+    use_cache: bool = False
+
+    _file_obj: BinaryIO = attrs.field(init=False)
+    _keys_offset_map: Dict[Hashable, _FilePos] = attrs.field(init=False)
+    _cache: Dict[Hashable, Union[Any, _Unloaded]] = attrs.field(init=False)
+
+    def __attrs_post_init__(self):
+        self._open()
+
+    def _open(self):
+        self._file_obj = open(self.path, "rb")
+        with open(PickledMapWriter.get_keys_offset_map_path(self.path), "rb") as f:
+            self._keys_offset_map = pickle.load(f)
+
+        if self.use_cache:
+            self._cache = {k: _Unloaded for k in self._keys_offset_map.keys()}
+        else:
+            self._cache = {}
+
+    def close(self):
+        self._file_obj.close()
+        self._cache.clear()
+        self._keys_offset_map.clear()
+
+    def __getitem__(self, key: Hashable) -> Any:
+        try:
+            offset = self._keys_offset_map[key]
+        except KeyError:
+            raise KeyError(f"Pickled map has no key {key}")
+
+        if self.use_cache and key in self._cache and self._cache[key] is not _Unloaded:
+            return self._cache[key]
+
+        self._file_obj.seek(offset)
+        obj = pickle.load(self._file_obj)
+        if self.use_cache:
+            self._cache[key] = obj
+
+        return obj
+
+    def __iter__(self) -> Iterator:
+        yield from self._keys_offset_map
+
+    def __len__(self) -> int:
+        return len(self._keys_offset_map)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+
+def delete_pickled_map(path: str):
+    """
+    Utility function to delete all files associated with a PickledCollection{Writer, Reader}.
+    """
+    if os.path.exists(path):
+        os.unlink(path)
+
+    keys_offset_map_path = PickledMapWriter.get_keys_offset_map_path(path)
+    if os.path.exists(keys_offset_map_path):
+        os.unlink(keys_offset_map_path)
 
 
 @attrs.define(eq=False, repr=False)
--- tests/utils/test_pickleutils.py
+++ tests/utils/test_pickleutils.py
@@ -61,3 +61,57 @@ class PickleUtilsTests(unittest.TestCase):
             self.assertEqual(60, reader[5])
 
         pickleutils.delete_pickled_collection(pickle_path)
+
+    def test_pickled_map_1(self):
+        pickle_path = os.path.join(paths.get_tmpdir_path(), "pickleutils_pm_test_1.pkl")
+        with pickleutils.PickledMapWriter(pickle_path) as writer:
+            writer["10"] = 10
+            writer["20"] = 20
+            writer["30"] = 30
+
+        with pickleutils.PickledMapReader(pickle_path) as reader:
+            self.assertEqual(10, reader["10"])
+            self.assertEqual(20, reader["20"])
+            self.assertEqual(30, reader["30"])
+
+        pickleutils.delete_pickled_map(pickle_path)
+
+    def test_pickled_map_2(self):
+        pickle_path = os.path.join(paths.get_tmpdir_path(), "pickleutils_pm_test_2.pkl")
+        with pickleutils.PickledMapWriter(pickle_path) as writer:
+            writer["10"] = 10
+            writer["20"] = 20
+            writer["30"] = 30
+
+        with pickleutils.PickledMapWriter(pickle_path, overwrite_existing=False) as writer:
+            writer["40"] = 40
+            writer["50"] = 50
+            writer["60"] = 60
+
+        with pickleutils.PickledMapReader(pickle_path) as reader:
+            self.assertEqual(6, len(reader))
+            self.assertEqual(10, reader["10"])
+            self.assertEqual(20, reader["20"])
+            self.assertEqual(30, reader["30"])
+            self.assertEqual(40, reader["40"])
+            self.assertEqual(50, reader["50"])
+            self.assertEqual(60, reader["60"])
+
+        pickleutils.delete_pickled_map(pickle_path)
+
+    def test_pickled_map_3(self):
+        pickle_path = os.path.join(paths.get_tmpdir_path(), "pickleutils_pm_test_2.pkl")
+        with pickleutils.PickledMapWriter(pickle_path) as writer:
+            writer["10"] = 10
+            writer["20"] = 20
+            writer["30"] = 30
+            self.assertRaises(ValueError, writer.__setitem__, "10", 10)
+
+        with pickleutils.PickledMapWriter(pickle_path, overwrite_existing=False) as writer:
+            writer["40"] = 40
+            writer["50"] = 50
+            writer["60"] = 60
+            self.assertRaises(ValueError, writer.__setitem__, "10", 10)
+            self.assertRaises(ValueError, writer.__setitem__, "40", 40)
+
+        pickleutils.delete_pickled_map(pickle_path)
