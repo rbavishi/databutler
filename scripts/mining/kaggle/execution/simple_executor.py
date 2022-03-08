1abadf8523bf0b847c0376d483aead30f46a0f8e
AMKS <arushisomani@berkeley.edu>
2022-03-07 22:08:51 -0800

    updating simple executor

--- scripts/mining/kaggle/execution/simple_executor.py
+++ scripts/mining/kaggle/execution/simple_executor.py
@@ -1,6 +1,9 @@
+import os
+
 from scripts.mining.kaggle.execution.base import BaseExecutor, register_runner
 from scripts.mining.kaggle.notebooks.notebook import KaggleNotebookSourceType
 
+from databutler.pat import astlib
 
 class SimpleExecutor(BaseExecutor):
     """
@@ -14,4 +17,21 @@ class SimpleExecutor(BaseExecutor):
         #  TODO: @Arushi: Write the source out to a file, and execute it appropriately using the source type.
         #        That is, determine if it is a notebook or a regular python file,
         #        and use the appropriate shell commands.
-        pass
+
+        if source_type == KaggleNotebookSourceType.IPYTHON_NOTEBOOK:
+            file_name = f'{output_dir_path}/source.ipynb'
+            # writing the file to file_name
+            with open(file_name, 'w') as f:
+                f.write(source)
+            print(f'Source written to {file_name}')
+            # executing file from terminal
+            os.system(f'jupyter nbconvert --execute {file_name} --to html')
+
+        elif source_type == KaggleNotebookSourceType.PYTHON_SOURCE_FILE:
+            file_name = f'{output_dir_path}/source.py'
+            # writing the file to file_name
+            with open(file_name, 'w') as f:
+                f.write(source)
+            print(f'Source written to {file_name}')
+            # executing file from terminal
+            os.system(f'python3 {file_name}')
