a125fc1b4a858f735072fa50ef09aca391e9cda5
Rohan Bavishi <rbavishi@berkeley.edu>
2022-06-19 16:52:39 -0700

    Add training and serving for embeddings-based engine

--- databutler/mining/kaggle/static_analysis/pandas_autodoc.py
+++ databutler/mining/kaggle/static_analysis/pandas_autodoc.py
@@ -28,8 +28,9 @@ from databutler.mining.kaggle.static_analysis.pandas_mining_utils import MinedRe
 from databutler.pat import astlib
 from databutler.utils import pickleutils, langmodels
 
-from sentence_transformers import SentenceTransformer, LoggingHandler, losses, InputExample, evaluation
+from sentence_transformers import SentenceTransformer, LoggingHandler, losses, InputExample, evaluation, util
 from torch.utils.data import DataLoader
+import torch
 
 ENGINE = 'code-davinci-002'
 PREPROCESSING_RESULTS_FILE = "pandas_mining_preprocessed.pkl"
@@ -788,10 +789,16 @@ def train_generational_model(campaign_dir: str, model_name: str, max_epochs: int
     simplet5_model.tokenizer.save_pretrained(os.path.join(campaign_dir, prefix + "_trained"))
 
 
-def prepare_dataset_for_training_embeddings(campaign_dir: str, per_equiv_class: int = 5):
+def prepare_dataset_for_training_embeddings(campaign_dir: str, per_equiv_class: int = 10):
     autodoc_successes_path = os.path.join(campaign_dir, AUTODOC_SUCCESSES_PATH)
     with pickleutils.PickledMapReader(autodoc_successes_path) as autodoc_reader:
         all_autodoc_results: List[AutodocResult] = list(tqdm.tqdm(autodoc_reader.values(), total=len(autodoc_reader)))
+
+    desc_by_uids: Dict[str, List[AutodocDescription]] = collections.defaultdict(list)
+    for res in all_autodoc_results:
+        for desc in res.correct_descriptions:
+            if desc.is_derived:
+                desc_by_uids[desc.uid].append(desc)
 
     non_derived_results: List[AutodocResult] = [res for res in all_autodoc_results
                                                 if all(not desc.is_derived for desc in res.correct_descriptions)]
@@ -799,18 +806,27 @@ def prepare_dataset_for_training_embeddings(campaign_dir: str, per_equiv_class: 
 
     equiv_classes: List[List[str]] = []
     for res in non_derived_results:
-        unique_nls = set(desc.nl for desc in res.correct_descriptions)
+        unique_nls: Set[str] = set()
+        for desc in res.correct_descriptions:
+            unique_nls.add(desc.nl)
+            if desc.llm_based_parameterization is not None:
+                unique_nls.add(desc.llm_based_parameterization.nl)
+
+            if len(desc_by_uids[desc.uid]) >= 1:
+                unique_nls.add(random.choice(desc_by_uids[desc.uid]).nl)
+
         unique_nls.update(desc.nl for desc in res.incorrect_descriptions)
         equiv_classes.append(list(unique_nls))
+        random.shuffle(equiv_classes[-1])
 
     all_indices: List[int] = list(range(len(equiv_classes)))
     correct_pairs: List[Tuple[str, str]] = []
     incorrect_pairs: List[Tuple[str, str]] = []
     for idx, equiv_class in enumerate(tqdm.tqdm(equiv_classes, desc="Preparing data")):
-        candidates = list(itertools.combinations(equiv_class, 2))
+        candidates = list(itertools.islice(itertools.combinations(equiv_class, 2), per_equiv_class))
         random.shuffle(candidates)
 
-        for s1, s2 in candidates[:per_equiv_class]:
+        for s1, s2 in candidates:
             correct_pairs.append((s1, s2))
 
         other_sample = random.sample(all_indices, per_equiv_class)
@@ -826,12 +842,12 @@ def prepare_dataset_for_training_embeddings(campaign_dir: str, per_equiv_class: 
     pickleutils.smart_dump([correct_pairs, incorrect_pairs], os.path.join(campaign_dir, "embedding_train_data.pkl"))
 
 
-def train_embeddings(campaign_dir: str):
+def train_embeddings(campaign_dir: str, loss_type: str = "contrastive", num_epochs: int = 5):
     correct_pairs, incorrect_pairs = pickleutils.smart_load(os.path.join(campaign_dir, "embedding_train_data.pkl"))
     random.shuffle(correct_pairs)
     random.shuffle(incorrect_pairs)
 
-    model = SentenceTransformer('all-MiniLM-L6-v2')
+    model = SentenceTransformer('all-mpnet-base-v2')
     train_examples = [
         *(InputExample(texts=[s1, s2], label=1) for s1, s2 in correct_pairs[:int(len(correct_pairs) * 0.8)]),
         *(InputExample(texts=[s1, s2], label=0) for s1, s2 in incorrect_pairs[:int(len(incorrect_pairs) * 0.8)]),
@@ -842,21 +858,117 @@ def train_embeddings(campaign_dir: str):
         *(InputExample(texts=[s1, s2], label=0) for s1, s2 in incorrect_pairs[int(len(incorrect_pairs) * 0.8):]),
     ]
 
+    if loss_type == "contrastive":
+        train_loss = losses.ContrastiveLoss(model=model)
+        output_path = os.path.join(campaign_dir, "embedding_model_contrastive")
+    elif loss_type == "cosine":
+        train_loss = losses.CosineSimilarityLoss(model=model)
+        output_path = os.path.join(campaign_dir, "embedding_model_cosine")
+        for ex in itertools.chain(train_examples, test_examples):
+            ex.label = float(ex.label)
+    else:
+        raise ValueError(f"Unsupported loss type: {loss_type}")
+
     evaluator = evaluation.EmbeddingSimilarityEvaluator.from_input_examples(test_examples, show_progress_bar=True)
 
     train_dataloader = DataLoader(train_examples, shuffle=True, batch_size=8)
-    train_loss = losses.ContrastiveLoss(model=model)
 
-    output_path = os.path.join(campaign_dir, "embedding_model")
     model.fit(
         [(train_dataloader, train_loss)],
         evaluator=evaluator,
-        epochs=5,
+        epochs=num_epochs,
         evaluation_steps=10000,
         show_progress_bar=True,
         save_best_model=True,
         output_path=output_path
     )
+
+
+def prepare_search_engine(campaign_dir: str, model_path: str):
+    model = SentenceTransformer(model_path)
+
+    autodoc_successes_path = os.path.join(campaign_dir, AUTODOC_SUCCESSES_PATH)
+    with pickleutils.PickledMapReader(autodoc_successes_path) as autodoc_reader:
+        all_autodoc_results: List[AutodocResult] = list(tqdm.tqdm(autodoc_reader.values(), total=len(autodoc_reader)))
+
+    corpus: List[AutodocDescription] = []
+    for res in all_autodoc_results:
+        for desc in res.correct_descriptions:
+            if desc.is_derived:
+                continue
+            corpus.append(desc)
+
+    print(f"Found {len(corpus)} autodoc descriptions")
+
+    embeddings = model.encode([desc.nl for desc in corpus], show_progress_bar=True, convert_to_tensor=True)
+    print("Finished generating embeddings")
+
+    embeddings_path = os.path.join(model_path, "search_engine_embeddings.pkl")
+    pickleutils.smart_dump((corpus, embeddings), embeddings_path)
+    print(f"Saved corpus and embeddings to {embeddings_path}")
+
+
+def start_search_engine(campaign_dir: str, model_path: str):
+    corpus, embeddings = pickleutils.smart_load(os.path.join(model_path, "search_engine_embeddings.pkl"))
+    print(f"Loaded corpus and embeddings from {model_path}")
+
+    model = SentenceTransformer(model_path)
+    while True:
+        query = input("Query: ")
+        query_embedding = model.encode(query, show_progress_bar=False, convert_to_tensor=True)
+        distances = util.cos_sim(query_embedding, embeddings)[0]
+
+        top_results = torch.topk(distances, k=100)
+        seen_code: Set[str] = set()
+        ctr = 0
+        for score, idx in zip(top_results[0], top_results[1]):
+            if corpus[idx].generated_code in seen_code:
+                continue
+
+            seen_code.add(corpus[idx].generated_code)
+            print(corpus[idx].nl, "(Score: {:.4f})".format(score))
+            print(corpus[idx].generated_code)
+            print("---")
+            ctr += 1
+
+            if ctr == 10:
+                break
+
+        print("\n----------\n")
+
+
+def run_search_engine(model_path: str, queries: List[str]):
+    corpus, embeddings = pickleutils.smart_load(os.path.join(model_path, "search_engine_embeddings.pkl"))
+    print(f"Loaded corpus and embeddings from {model_path}")
+
+    model = SentenceTransformer(model_path)
+    all_results: List[List[Dict]] = []
+    for query in tqdm.tqdm(queries, desc="Processing queries"):
+        query_embedding = model.encode(query, show_progress_bar=False, convert_to_tensor=True)
+        distances = util.cos_sim(query_embedding, embeddings)[0]
+
+        top_results = torch.topk(distances, k=100)
+        seen_code: Set[str] = set()
+        ctr = 0
+        results: List[Dict] = []
+        for score, idx in zip(top_results[0], top_results[1]):
+            if corpus[idx].generated_code in seen_code:
+                continue
+
+            seen_code.add(corpus[idx].generated_code)
+            results.append({
+                "score": score,
+                "nl": corpus[idx].nl,
+                "code": corpus[idx].generated_code
+            })
+            ctr += 1
+
+            if ctr == 10:
+                break
+
+        all_results.append(results)
+
+    return all_results
 
 
 def analyze(campaign_dir: str):
@@ -885,4 +997,6 @@ if __name__ == "__main__":
         "train_generational_model": train_generational_model,
         "prepare_dataset_for_training_embeddings": prepare_dataset_for_training_embeddings,
         "train_embeddings": train_embeddings,
+        "prepare_search_engine": prepare_search_engine,
+        "start_search_engine": start_search_engine,
     })
