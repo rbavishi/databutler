9d0ea34351d0f648677754c68faef10311f23301
Rohan Bavishi <rbavishi@berkeley.edu>
2022-09-10 15:08:46 -0700

    Add script for training embeddings and generational model baselines

---
+++ embedding_training_script.sh
@@ -0,0 +1,2 @@
+python databutler/mining/kaggle/static_analysis/pandas_autodoc.py train_embeddings ./scratch/pandas_mining_05_27 cosine 1 
+python databutler/mining/kaggle/static_analysis/pandas_autodoc.py train_embeddings ./scratch/pandas_mining_05_27 contrastive 1
---
+++ gen_model_training.sh
@@ -0,0 +1,2 @@
+python experiments/jigsaw_experiments.py GenerationalModelWithSearchBaseline --campaign-dir="./scratch/pandas_mining_06_22" train_generational_model 10
+python experiments/jigsaw_experiments.py GenerationalModelBaseline --campaign-dir="./scratch/pandas_mining_06_22" train_generational_model 10
